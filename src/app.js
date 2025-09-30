03d521ce85d639389dcea27860ce9a99d9547836
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2025-09-30 13:36:02 +0530

    Revert CORS origin setting to allow all origins

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:3001',
+  origin: '*',
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
